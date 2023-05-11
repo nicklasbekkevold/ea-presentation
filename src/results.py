10742b0cecefe2ca791d52f9c3f321589ddb2d84
Nicklas Bekkevold <44287251+nicklasbekkevold@users.noreply.github.com>
2023-05-11 14:11:54 +0200

    Add result folder functions

---
+++ src/results.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+RESULTS_FOLDER = "results"
+PARAMETERS_PATH = "src/parameters.py"
+
+
+def get_latest_result_folder_number(path=RESULTS_FOLDER) -> int:
+    if not os.path.exists(path):
+        return 0
+
+    all_folders = os.listdir(path)
+    all_folders.sort()
+    return int(all_folders[-1])
+
+
+def get_current_result_folder(path=RESULTS_FOLDER) -> str:
+    return f"{path}/{get_latest_result_folder_number()}"
+
+
+def create_result_folder(path=RESULTS_FOLDER) -> None:
+    latest_result = get_latest_result_folder_number(path)
+    os.makedirs(f"{RESULTS_FOLDER}/{latest_result + 1}")
+
+
+def copy_parameters(path=RESULTS_FOLDER) -> None:
+    latest_result_folder = get_current_result_folder(path)
+    destination = f"{latest_result_folder}/parameters.py"
+    shutil.copyfile(PARAMETERS_PATH, destination)
